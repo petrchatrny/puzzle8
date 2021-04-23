7f0c8311551210112129075de1a25d2b294d79d4
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2021-04-23 16:10:43 +0200

    navigation

--- app/src/main/java/com/github/petrchatrny/puzzle8/view/MainActivity.kt
+++ app/src/main/java/com/github/petrchatrny/puzzle8/view/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.github.petrchatrny.puzzle8.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.github.petrchatrny.puzzle8.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // setup navigation
+        mainBottomNav.setupWithNavController(navHostFragment.findNavController())
     }
 }
