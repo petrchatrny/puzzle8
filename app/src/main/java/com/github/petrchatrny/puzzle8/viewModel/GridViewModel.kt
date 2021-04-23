b420b78bee645cb26614ff4486c0272a0eccdfab
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2021-04-23 11:19:25 +0200

    android init
    initialize all android documents

---
+++ app/src/main/java/com/github/petrchatrny/puzzle8/viewModel/GridViewModel.kt
@@ -0,0 +1,7 @@
+package com.github.petrchatrny.puzzle8.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class GridViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/github/petrchatrny/puzzle8/viewModel/HistoryViewModel.kt
@@ -0,0 +1,7 @@
+package com.github.petrchatrny.puzzle8.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class HistoryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
